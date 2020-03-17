6a5fe36a1a9af0b6e662f66a572ecdf571b789cc
Emmanuel Thomas Devasia <50878033+emmanuel010@users.noreply.github.com>
2020-03-18 02:20:54 +0530

    changed to bitcoin.js also changed app.js to include grpc calls

--- app.js
+++ app.js
@@ -1,48 +1,113 @@
 const express = require('express');
-//const grpc = require('grpc');
-
+const grpc = require('grpc');
+var bodyParser = require('body-parser');
+const services = require('../bitcoin-web/protobuf/APIs_grpc_pb');
+const dataStr = require('../bitcoin-web/protobuf/APIs_pb');
 const app = express();
-const tokens = ["91542a37db764d1c8e499404862c7ccd","a0d480b076774357baab2329be7b308d","1f04a3d4f6434c87ad5ef58daa34c4bd","f1a46886248e4d629cb68fe0ba681329"];
+const tokens = ["91542a37db764d1c8e499404862c7ccd", "a0d480b076774357baab2329be7b308d", "1f04a3d4f6434c87ad5ef58daa34c4bd", "f1a46886248e4d629cb68fe0ba681329"];
+
 let tokenIterator = 0;
 app.use(express.static("public"));
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/index.html")
-})
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+});
 // For getting token after round robin algorithm from a list
-app.get('/getToken',function (request,response) {
+app.get('/getToken', function (request, response) {
     tokenIterator = tokenIterator +1;
     let index = tokenIterator%4;
     if (index>3){
         response.send((tokens[3]).toString());
     }else{
         response.send((tokens[index]).toString());
-        console.log(tokens[index])
+        // console.log(tokens[index])
     }
 });
 
-app.get('/getPebblePublicKey',function (request,response) {
-    console.log(request);
+app.get('/getPebblePublicKey', function (request, response) {
+    // console.log(request);
     response.send("021a46d92df358af9acce575810581b4cc24fc9387c5ef073efefb83a6421beae4");
 });
 
-app.get('/registerJointAccount',function (request,response) {
+//the function called on to initialize the account
+app.post('/start', function (request, response) {
     console.log(request);
-    response.send("request.body");
+    //let respArray=callGauthamforTransactions();
+    let respArray = [{
+        toAddress: "toAddressfromapp.js",
+        amount: "$36,738",
+        transactionID: "021a46d92df358af9acce575810581b4cc24fc9387c5ef073efefb83a6421beae4",
+        status: 'success',
+        fromAddress: "fromAddressfromapp.js"
+    },
+        {
+            toAddress: "toAddressfromapp.js",
+            amount: "$36,738",
+            transactionID: "2",
+            status: 'success',
+            fromAddress: "fromAddressfromapp.js"
+        },
+        {
+            toAddress: "toAddressfromapp.js",
+            amount: "$36,738",
+            transactionID: "3",
+            status: 'success',
+            fromAddress: "fromAddressfromapp.js"
+        }
+    ];
+    let res = {arr: respArray};
+    response.send(JSON.stringify(res));
 });
 app.get('/updateBalance',function (request,response) {
-    console.log(request);
+    //console.log(request);
     response.send(request.body);
 });
-app.get('/createTransaction',function (request,response) {
-    console.log(request);
+app.get('/createTransaction', function (request, response) {
+    //console.log(request);
     response.send(request.body);
 });
-app.get('/withdrawBitcoins',function (request,response) {
-    console.log(request);
+app.get('/withdrawBitcoins', function (request, response) {
+    // console.log(request);
     response.send(request.body);
 });
 
-const server = app.listen(5030,function () {
-console.log("API Gateway listening on port %d", server.address().port);
+//calling grpc api
+function callGauthamforTransactions(multiAddr, withdrawAddress, networkType) {
+    let client = new services.PebbleBTCClient(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+    );
+    let request = new dataStr.Account();
+    request.setJointaccountaddress(multiAddr);
+    request.setWithdrawingaddress(withdrawAddress);
+    request.setNetworktype(networkType);
+    let call = client.registerJointAccount(request, () => {
+    });
+    let arr = [];
+    call.on("data", res => {
+        arr.push({
+            toAddress: res.getToaddress(),
+            amount: res.getAmount(),
+            unixTimestamp: res.getUnixtimestamp(),
+            transactionID: res.getTransactionid(),
+            pebbleNodeSignatures: res.getPebblenodesignaturesList(),
+            status: res.getStatus(),
+            fromAddress: res.getFromaddress(),
+            vectorClock: res.getVectorclockList(),
+            commitmentPayload: res.getCommitmentpayloadList(),
+            networkType: res.getNetworktype(),
+            commitmentTransaction: res.getCommitmenttransaction(),
+            internalTransaction: res.getInternaltransaction(),
+            encodedDatatoSign: res.getEncodeddatatosign(),
+            Withdrawn: res.getWithdrawn(),
+            userPublickeySignature: res.getUserpublickeysignature()
+        });
+    });
+    call.on("error", error => {
+        console.log(error)
+    });
+    return arr;
+}
+
+const server = app.listen(5030, function () {
+    console.log("API Gateway listening on port %d", server.address().port);
 });
--- public/bitcoin.js
+++ public/bitcoin.js
@@ -1,103 +1,144 @@
-"use strict";
-function start() {
-    document.getElementById("balance").innerHTML = "12121212121";
-    const global_var = {
-        token: "",
-        pebblePublicKey: "",
-        api_endpoint: "https://api.blockcypher.com/v1/btc/",
-    };
-    fetch('/getToken')
-        .then(res => res.text()
-        ).then((res) => {
-        global_var.token = res
+const global_var = {
+    token: "",
+    passPhrase: "",
+    withdrawAddress: "",
+    api_endpoint: "https://api.blockcypher.com/v1/btc/",
+};
+fetch('/getToken')
+    .then(res => res.text()
+    ).then((res) => {
+    global_var.token = res
+});
+fetch('/getPebblePublicKey')
+    .then(res => res.text())
+    .then(res => {
+        //console.log(res)
+        global_var.pebblePublicKey = res
     });
-    fetch('/getPebblePublicKey')
-        .then(res => res.text())
-        .then(res => {
-            //console.log(res)
-            global_var.pebblePublicKey = res
-        });
 
-    // console.log(global_var)
+// console.log(global_var)
 
 
+Swal.fire({
+    grow: 'fullscreen',
+    title: 'Welcome To Pebble',
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    text: 'Select Network',
+    footer: 'Get ready for the fastest DLT in the World',
+    imageUrl: "pebble-icon.svg",
+    input: 'select',
+    inputOptions: {
+        main: 'Bitcoin Main Network',
+        test3: 'Bitcoin Test Network 3',
+    },
+    confirmButtonText: 'Proceed',
+    inputValidator: (value) => {
+        return new Promise((resolve) => {
+            resolve()
+        })
+    }
+}).then((res) => {
+    let netType = res.value;
+    global_var.networkType = netType;
+    //console.log(global_var);
     Swal.fire({
-        grow: 'fullscreen',
-        title: 'Welcome To Pebble',
+
         allowOutsideClick: false,
         allowEscapeKey: false,
-        text: 'Select Network',
-        footer: 'Get Ready for the Fastest DLT in the World',
-        imageUrl: "pebble-icon.svg",
-        input: 'select',
-        inputOptions: {
-            main: 'Bitcoin Main Network',
-            test3: 'Bitcoin Test Network 3',
-        },
-        confirmButtonText: 'Proceed',
-        inputValidator: (value) => {
-            return new Promise((resolve) => {
-                resolve()
-            })
-        }
-    }).then((res) => {
-        let netType = res.value;
-        global_var.networkType = netType;
-        //console.log(global_var);
-        let arr = Swal.fire({
+        grow: 'fullscreen',
+        confirmButtonText: 'Submit',
+        title: "",
 
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            grow: 'fullscreen',
-            confirmButtonText: 'Submit',
-            title: "",
-
-            html:
-                '<div class="label"><span title="What do u wanna write here?"> Enter Public Key</span></div>' +
-                '<form><input required="required" id="swal-input1" class="swal2-input" style="width: 40%"> <br>',
+        html:
+            '<video id="preview" width="320" height="240"></video>' +
+            '<div class="label"><span title="What do u wanna write here?"><h5>Enter Passphrase:</h5> </span></div>' +
+            '<form><input required="required" id="swal-input1" class="swal2-input" style="width: 40%"> <br>' +
+            '<div class="label"><span title="What do u wanna write here?"><h5>Enter Withdraw Address:</h5> </span></div>' +
+            '<form><input required="required" id="swal-input2" class="swal2-input" style="width: 40%"> <br>' +
+            '<button type="button" id="scan" onclick="scanner()" class="swal2-styled swal2-cancel">Scan Withdraw Address</button>',
 
 
-            footer: '<button type="button" id="newKey" class="swal2-confirm swal2-styled" onclick="generatePublicKey()" style="display: inline-block; border-left-color: rgb(48, 133, 214); border-right-color: rgb(48, 133, 214); background-color:#5cb85c;">Generate New Public Key</button>',
-
-            preConfirm: () => {
-                global_var.userPublicKey = document.getElementById('swal-input1').value;
-                // generateMultisignAddress();
+        preConfirm: () => {
+            global_var.passPhrase = document.getElementById('swal-input1').value;
+            global_var.withdrawAddress = document.getElementById('swal-input2').value;
+            //generateMultisignAddress();
+            if (!(global_var.withdrawAddress)) {
+                Swal.showValidationMessage(
+                    "Withdraw Address cannot be left blank"
+                )
+            } else if (!(global_var.passPhrase)) {
+                Swal.showValidationMessage(
+                    "Passphrase cannot be left blank"
+                )
+            } else {
                 return {
-
                     networkType: netType,
                     publicKey: document.getElementById('swal-input1').value,
                 }
             }
+        },
+    });
 
+
+}).then((res) => {
+
+    console.log(global_var)
+
+});
+
+
+function scanner() {
+    let scanner = new Instascan.Scanner({video: document.getElementById('preview')});
+    scanner.addListener('scan', function (content) {
+
+        content = content.replace("bitcoin:", "");
+        document.getElementById('swal-input2').value = content;
+        scanner.stop();
+        document.getElementById("preview").style.display = "none";
+        scanner.stop();
+
+
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+
+}
+
+
+function generateMultisignAddress() {
+    swal.fire({
+        title: 'Loading',
+        html: 'Please Wait',
+        onBeforeOpen: () => {
+            Swal.showLoading()
+        }
+    });
+    const data = {
+        "pubkeys": [
+            global_var.pebblePublicKey,
+            global_var.userPublicKey
+
+        ],
+        "script_type": "multisig-2-of-2"
+    };
+
+    $.post(global_var.api_endpoint + global_var.networkType + '/addrs?token=' + global_var.token, JSON.stringify(data))
+        .then(function (d) {
+            console.log(d.address);
+            document.getElementById("userAddress").innerHTML = d.address;
+            $.get(global_var.api_endpoint + global_var.networkType + '/addrs/' + d.address + '/balance?token=' + global_var.token)
+                .then(function (d) {
+                    document.getElementById("unConfirmedBalance").innerHTML = '$' + d.unconfirmed_balance;
+                    document.getElementById("balance").innerHTML = '$ ' + d.balance;
+                    swal.close()
+                });
         });
-
-        return arr
-    }).then((res) => {
-        console.log(res)
-        console.log(global_var)
-    })
-
-    function generateMultisignAddress() {
-
-        const data = {
-            "pubkeys": [
-                global_var.pebblePublicKey,
-                global_var.userPublicKey
-
-            ],
-            "script_type": "multisig-2-of-2"
-        };
-
-        $.post(global_var.api_endpoint + global_var.networkType + '/addrs?token=' + global_var.token, JSON.stringify(data))
-            .then(function (d) {
-                console.log(d.address);
-                document.getElementById("userAddress").innerHTML = d.address;
-                $.get(global_var.api_endpoint + global_var.networkType + '/addrs/' + d.address + '/balance?token=' + global_var.token)
-                    .then(function (d) {
-                        document.getElementById("unConfirmedBalance").innerHTML = '$' + d.unconfirmed_balance;
-                        document.getElementById("balance").innerHTML = '$ ' + d.balance;
-                    });
-            });
-    }
-
 }

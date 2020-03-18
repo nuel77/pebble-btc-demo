9dfd1cb0c0ecc0e29d7d4b7211a2b7233df4ae6e
Emmanuel Thomas Devasia <50878033+emmanuel010@users.noreply.github.com>
2020-03-18 13:46:46 +0530

    okeda

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const grpc = require('grpc');
 var bodyParser = require('body-parser');
-const services = require('../bitcoin-web/protobuf/APIs_grpc_pb');
-const dataStr = require('../bitcoin-web/protobuf/APIs_pb');
+//const services = require('../bitcoin-web/protobuf/APIs_grpc_pb');
+//const dataStr = require('../bitcoin-web/protobuf/APIs_pb');
+const services = require('../pebble-btc-demo/protobuf/APIs_grpc_pb');
+const dataStr = require('../pebble-btc-demo/protobuf/APIs_pb');
 const app = express();
 const tokens = ["91542a37db764d1c8e499404862c7ccd","a0d480b076774357baab2329be7b308d","1f04a3d4f6434c87ad5ef58daa34c4bd","f1a46886248e4d629cb68fe0ba681329"];
 
@@ -11,6 +13,8 @@ app.use(express.static("public"));
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/index.html")
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // For getting token after round robin algorithm from a list
 app.get('/getToken',function (request,response) {
     tokenIterator = tokenIterator +1;
@@ -24,36 +28,38 @@ app.get('/getToken',function (request,response) {
 });
 
 app.get('/getPebblePublicKey',function (request,response) {
-   // console.log(request);
+
     response.send("021a46d92df358af9acce575810581b4cc24fc9387c5ef073efefb83a6421beae4");
 });
 
 //the function called on to initialize the account
 app.post('/start',function (request,response) {
-    console.log(request);
-    //let respArray=callGauthamforTransactions();
-    let respArray = [{
-        toAddress: "toAddressfromapp.js",
-        amount: "$36,738",
-        transactionID: "021a46d92df358af9acce575810581b4cc24fc9387c5ef073efefb83a6421beae4",
-        status: 'success',
-        fromAddress: "fromAddressfromapp.js"
-    },
-        {
-            toAddress: "toAddressfromapp.js",
-            amount: "$36,738",
-            transactionID: "2",
-            status: 'success',
-            fromAddress: "fromAddressfromapp.js"
-        },
-        {
-            toAddress: "toAddressfromapp.js",
-            amount: "$36,738",
-            transactionID: "3",
-            status: 'success',
-            fromAddress: "fromAddressfromapp.js"
-        }
-    ];
+
+    let msg=request.body;
+    console.log(msg);
+    let respArray=callGauthamforTransactions(msg.multiAdrr,msg.withdrawAddress,msg.networkType);
+    // let respArray = [{
+    //     toAddress: "toAddressfromapp.js",
+    //     amount: "$36,738",
+    //     transactionID: "021a46d92df358af9acce575810581b4cc24fc9387c5ef073efefb83a6421beae4",
+    //     status: 'success',
+    //     fromAddress: "fromAddressfromapp.js"
+    // },
+    //     {
+    //         toAddress: "toAddressfromapp.js",
+    //         amount: "$36,738",
+    //         transactionID: "2",
+    //         status: 'success',
+    //         fromAddress: "fromAddressfromapp.js"
+    //     },
+    //     {
+    //         toAddress: "toAddressfromapp.js",
+    //         amount: "$36,738",
+    //         transactionID: "3",
+    //         status: 'success',
+    //         fromAddress: "fromAddressfromapp.js"
+    //     }
+    // ];
     let res = {arr: respArray};
     response.send(JSON.stringify(res));
 });
@@ -73,7 +79,7 @@ app.get('/withdrawBitcoins',function (request,response) {
 //calling grpc api
 function  callGauthamforTransactions(multiAddr,withdrawAddress,networkType) {
     let client = new services.PebbleBTCClient(
-        'localhost:50051',
+        'localhost:5031',
         grpc.credentials.createInsecure()
     );
     let request=new dataStr.Account();
